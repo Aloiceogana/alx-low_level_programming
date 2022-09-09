4c43518c722b3ebb03a38104bf1663e6fb79f982
Aloiceogana <aloiceadriot@gmail.com>
2022-09-08 18:07:53 -0700

    Size is not grandeu

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print the string in the put function
+ *
+ * Describe program prints "Programming is like building a multilingual puzzle 
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
+	printf("size of an int: %1d byte(s)\n", sizeof(i));
+	printf("size of a long int: %ld bytes(s)\n", sizeof(lli));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("size of a float: %ld byte(s))\n" sizeof(f));
+	return (0);
+}
