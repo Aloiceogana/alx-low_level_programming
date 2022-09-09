934e868ad51fc31bbc62eddfc5a25763af17d293
Aloiceogana <aloiceadriot@gmail.com>
2022-09-08 18:16:47 -0700

    Size is not grandeu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * print the string in the put function
+ * main - print the string in the put function
  *
- * Describe program prints "Programming is like building a multilingual puzzle 
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle 
  * Return: 0
  */
 int main(void)
