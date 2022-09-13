7fba8a4abd87356a35f38e8e917f572d17a8a8db
Aloiceogana <aloiceadriot@gmail.com>
2022-09-13 10:56:00 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,8 @@
 #ifndef HOLBERTON_H_INCLUDED                                                                                                          
 #define HOLBERTON_H_INCLUDED                                                                                                          
                                                                                                                                       
-int _putchar(char);                                                                                                                    
-void print_alphabet(void);
-void print_alphabet_×10(void);
+int _putchar(char);                                                                                                                   void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
