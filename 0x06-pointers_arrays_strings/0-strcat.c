d04ce4649b23cad69237bccfd10f7874b87d4b40
Aloiceogana <aloiceadriot@gmail.com>
2022-09-21 14:37:34 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,13 @@
 char *_streat(char *dest, char *src)
 {
 
-int index = 0, dest_len = 0;
+        int index = 0, dest_len = 0;
 
         while(dest[index++])
 		dest_len++;
 
 	for (index = 0; sre[index]; index++)
-		dest[dest_len++] = sre[index];
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
